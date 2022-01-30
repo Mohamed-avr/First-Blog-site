3d62d0f0aba9d4343c3215ce6ee55f7f6a4cc476
Mohamed-avr <mouh4945@gmail.com>
2022-01-30 18:00:31 +0100

    11ty-fied-the-static-pages

--- .eleventy.js
+++ .eleventy.js
@@ -20,28 +20,3 @@ module.exports  = function (eleventyConfig)  {
     }; 
 } 
 
-// const  buttonNavbar = document.querySelector(".menu-ham");
-// const navBarContainer = document.querySelector(".topNav");
-// const navFexedChild =  navBarContainer.firstElementChild ;
-// // selectChildren
-// const childOne = document.querySelector('.oneX')
-// const childTow = document.querySelector('.towX')
-// const childThree = document.querySelector('.threeX');
-
-
-
-// // {#  the event  #}
-// buttonNavbar.addEventListener('click' , function() {
-
-//     childOne.classList.toggle("one"); 
-//     childTow.classList.toggle("tow"); 
-//     childThree.classList.toggle("three"); 
-
-
-//     if( childOne.classList.contains("one")) {
-//       alert('here you can set your script')
-//     }
-
-
-// })
-
